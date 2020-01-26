2e5d441845a37010a50bd0af45c88733d7844f80
prince3103 <prince.sethi@trantorinc.com>
2020-01-26 18:08:32 +0530

    Soft Keyboard hide functionality

--- src/main/java/com/example/cvrecommendation/LetsGetStarted.java
+++ src/main/java/com/example/cvrecommendation/LetsGetStarted.java
@@ -8,12 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Process;
-import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -23,6 +17,8 @@ import android.widget.Toast;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Objects;
 
 import okhttp3.MediaType;
@@ -38,6 +34,7 @@ public class LetsGetStarted extends AppCompatActivity {
 
     TextView welcomeTextView;
     Intent file_choose_intent;
+    String access_token="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +43,7 @@ public class LetsGetStarted extends AppCompatActivity {
 
         welcomeTextView = findViewById(R.id.welcomeTextView);
 
+        access_token="";
         welcomeScreen();
     }
 
@@ -54,6 +52,7 @@ public class LetsGetStarted extends AppCompatActivity {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         if (getCurrentFocus() != null) {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert imm != null;
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
         return super.dispatchTouchEvent(ev);
@@ -72,8 +71,7 @@ public class LetsGetStarted extends AppCompatActivity {
                 e.printStackTrace();
             }
             super.onBackPressed();
-            return;
-    }
+        }
 
     public void welcomeScreen(){
 
@@ -83,9 +81,10 @@ public class LetsGetStarted extends AppCompatActivity {
         String user_name="";
 
         if (extras != null) {
-            user_name = intent.getExtras().getString("user_name", "guest");
+            user_name = Objects.requireNonNull(intent.getExtras()).getString("user_name", "guest");
+            access_token = intent.getExtras().getString("access_token", "guest");
         }
-        welcomeTextView.setText("Welcome "+user_name);
+        welcomeTextView.setText(String.format("Welcome %s", user_name));
     }
 
     public void chooseFile(View view){
@@ -96,44 +95,62 @@ public class LetsGetStarted extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        String file_path="";
-        switch (requestCode){
-            case 10:
-                if(resultCode==RESULT_OK){
-                    file_path = Objects.requireNonNull(data.getData()).getPath();
-                    welcomeTextView.setText(file_path);
-                }
-
-                break;
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK) {
+                assert data != null;
+                String file_path = data.getData().getPath();
+                System.out.print(file_path);
+//                    Uri file_uri= Uri.fromFile(new File(file_path));
+                upload_file(file_path);
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Please try again", Toast.LENGTH_SHORT).show();
         }
-        Uri file_uri= Uri.fromFile(new File(file_path));
-        upload_file(file_path);
+
     }
 
+
+
     public void upload_file(String file_path){
-//            //---------------------------------------------
+        System.out.println("upload file function call");
         //Retrofit Class defining base url where app needs to post data
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://job-recommendation-api.herokuapp.com/")
                 .build();
         //Retrofit class generates an implementation of Api interface
         Api api = retrofit.create(Api.class);
+        Uri file_uri= Uri.fromFile(new File(file_path));
 
             //pass it like this
-            File file = new File(file_path);
+
+//            File file = new File(file_path);
+
+        File file = new File(getCacheDir(), "hello.txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.append("Hello");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(file);
+        System.out.println(file.getName());
+
             RequestBody requestFile =
                     RequestBody.create(MediaType.parse("multipart/form-data"), file);
 
             // MultipartBody.Part is used to send also the actual file name
                     MultipartBody.Part body =
-                            MultipartBody.Part.createFormData("pdf", file.getName(), requestFile);
+                            MultipartBody.Part.createFormData("file", file.getName(), requestFile);
 
             // add another part within the multipart request
-                    RequestBody description =
-                            RequestBody.create(MediaType.parse("multipart/form-data"), "myPdf");
-
+                    RequestBody access_token_saved =
+                            RequestBody.create(MediaType.parse("multipart/form-data"), access_token);
+                    access_token+="JWT "+access_token;
         //This method POST request and we receive return data in response
-        api.uploadcv(description, body).enqueue(new Callback<ResponseBody>() {        //add access token
+        api.uploadcv(access_token, body).enqueue(new Callback<ResponseBody>() {        //add access token
             //Method executed if we receive response from server
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
@@ -143,17 +160,20 @@ public class LetsGetStarted extends AppCompatActivity {
                         String api_response = response.body().string();
                         System.out.println(api_response);
                         JSONObject jsonOb = new JSONObject(api_response);
-//                        String access_token = jsonOb.optString("access_token");
-//                        System.out.println(access_token);
+                        String message_received = jsonOb.optString("message");
+                        System.out.println(message_received);
+                        welcomeTextView.setText(message_received);
                     } else {
-                        Toast.makeText(getApplicationContext(), "Invalid email_address or Password", Toast.LENGTH_SHORT).show();
-//                        loginButton.setClickable(true);
+                        Toast.makeText(getApplicationContext(), "File Not Uploaded. Please try again", Toast.LENGTH_SHORT).show();
+                        System.out.println("File Not Uploaded. Please try again");
+                        welcomeTextView.setText("File Not Uploaded. Please try again");
                     }
 
                 } catch (Exception e) {
                     System.out.println("response catch exception : " + e);
+                    welcomeTextView.setText("response catch exception : " + e);
                     e.printStackTrace();
-//                    loginButton.setClickable(true);
+
 
                 }
 
@@ -165,8 +185,11 @@ public class LetsGetStarted extends AppCompatActivity {
                 if (t.toString().equals("java.net.UnknownHostException:" +
                         " Unable to resolve host \"prediction1-rest-api.herokuapp.com\": No address associated with hostname")) {
                     Toast.makeText(getApplicationContext(), "Please Check Your Network Connection", Toast.LENGTH_SHORT).show();
+                    welcomeTextView.setText("Please Check Your Network Connection");
                 } else {
                     Toast.makeText(getApplicationContext(), "Server Not Responding. Please Try Again, Later.", Toast.LENGTH_SHORT).show();
+                    welcomeTextView.setText(t.toString());
+                    System.out.println(t);
                 }
 //                loginButton.setClickable(true);
             }
